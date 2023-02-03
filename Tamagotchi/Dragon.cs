7ad46c01a8fb0e2c7b2a8204f8ee447df4718d16
adamhrapkowicz <wolsztyn@hotmail.com>
2023-02-03 10:18:42 +0000

    Add project files.

---
+++ Tamagotchi/Dragon.cs
@@ -0,0 +1,14 @@
+﻿namespace Tamagotchi
+{
+    internal class Dragon
+    {
+        public Dragon()
+        {
+
+        }
+
+        public bool IsAlive { get; internal set; }
+        public int Feedometer { get; internal set; }
+        public int Happiness { get; internal set; }
+    }
+}
---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Tamagotchi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dragon = new Dragon();
+            dragon.IsAlive = true;
+            dragon.Happiness = 5;
+            dragon.Feedometer = 1;
+            while (dragon.IsAlive)
+            {
+                if (dragon.Happiness == 0 || dragon.Feedometer == 0)
+                {
+                    dragon.IsAlive = false;
+                }
+                //Console.WriteLine("To check age press a, to check feedometer press f, to check if it is happy press h");
+                Console.WriteLine("To feed press 1, to pet press 2");
+                var usersAction = Console.ReadLine().ToString();
+                if (usersAction == "1") 
+                {
+                    dragon.Feedometer += 1;
+                }
+                else if (usersAction == "2")
+                {
+                    dragon.Happiness += 1;
+                }
+                else
+                {
+                    Console.WriteLine($"You pressed the wrong button. Your dragon didn't get fed and wasn't petted");
+                }
+            }
+        }
+    }
+}
