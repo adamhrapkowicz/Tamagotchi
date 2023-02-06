2ee984f9d073e17655af90743e00621bbd5a4ff0
adamhrapkowicz <wolsztyn@hotmail.com>
2023-02-06 15:15:32 +0000

    Threads added to decrease happiness and feedometer and allowe user to feed and pet dragon.
    2 new threads created to LetUserFeedAndPetDragon and to DecreaseFeedometerAndHappiness. FeedometerAndHappinessDecreaser class removed.

--- Tamagotchi/LifeCycle.cs
+++ Tamagotchi/LifeCycle.cs
@@ -1,38 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tamagotchi
+﻿namespace Tamagotchi
 {
     public class LifeCycle
     {
-        public static void RunLifeCycle(Dragon dragon) 
+        public static void RunLifeCycle(Dragon dragon)
         {
-            while (dragon.IsAlive)
+            Thread letUserFeedAndPetDragonThread = new Thread(LetUserFeedAndPetDragon(dragon));
+            Thread decreaseFeedometerAndHappinessThread = new Thread(DecreaseFedometerAndHappiness(dragon));
+            letUserFeedAndPetDragonThread.Start();
+            decreaseFeedometerAndHappinessThread.Start();
+        }
+
+       
+        public static ParameterizedThreadStart LetUserFeedAndPetDragon(Dragon dragon)
+        {
+            while (true)
             {
-                //Thread thread1 = new Thread(Decr);
-                Console.WriteLine($"Value of happiness is {dragon.Happiness} and value of feedometer is {dragon.Feedometer}");
-                //if (dragon.Happiness == 0 || dragon.Feedometer == 0)
-                //{
-                //    dragon.IsAlive = false;
-                //}
-                //Console.WriteLine("To check age press a, to check feedometer press f, to check if it is happy press h");
-                Console.WriteLine("To feed press 1, to pet press 2");
-                var usersAction = Console.ReadLine()?.ToString();
-                if (usersAction == "1")
+                while (dragon.IsAlive)
                 {
-                    dragon.Feedometer += 1;
+                    Console.WriteLine($"Value of happiness is {dragon.Happiness} and value of feedometer is {dragon.Feedometer}.");
+                    
+                    Console.WriteLine("To feed press 1, to pet press 2.");
+                    var userAction = Console.ReadLine();
+                    if (userAction == "1")
+                    {
+                        dragon.Feedometer++;
+                        Console.WriteLine($"That was yummy!");
+                    }
+                    else if (userAction == "2")
+                    {
+                        dragon.Happiness++;
+                        Console.WriteLine($"I love you!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You pressed the wrong button. Your dragon didn't get fed and wasn't petted.");
+                    }
                 }
-                else if (usersAction == "2")
-                {
-                    dragon.Happiness += 1;
-                }
-                else
-                {
-                    Console.WriteLine($"You pressed the wrong button. Your dragon didn't get fed and wasn't petted");
-                }
+               
+
+            }
+        }
+
+        public static ParameterizedThreadStart DecreaseFedometerAndHappiness(Dragon dragon)
+        {
+            while (true)
+            {
+                dragon.Feedometer--;
+                dragon.Happiness--;
+                Console.WriteLine($"Value2 of happiness is {dragon.Happiness} and value of feedometer is {dragon.Feedometer}.");
+                Thread.Sleep(6000);
             }
         }
     }
