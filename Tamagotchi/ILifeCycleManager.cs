fc5c114c90fe8be31b90b8951ddb43226beeb230
adamhrapkowicz <wolsztyn@hotmail.com>
2023-02-10 15:09:47 +0000

    Feat: FeedometerIncrement rename to CareLevelManager() returning now array of ints to handle increase of Feedometer and Happiness after care instructions from user and decrease of Feedometer and Happiness as life progress according to levels for each AgeGroup using switch statment.

--- Tamagotchi/ILifeCycleManager.cs
+++ Tamagotchi/ILifeCycleManager.cs
@@ -3,7 +3,11 @@
     internal interface ILifeCycleManager
     {
         string IncreaseFeedometer(Dragon dragon);
+
         string IncreaseHappiness(Dragon dragon);
+        
         void ProgressLifeSettings(Dragon dragon);
+        
+        int[] CareLevelManager(Dragon dragon);
     }
 }
--- Tamagotchi/LifeCycleManager.cs
+++ Tamagotchi/LifeCycleManager.cs
@@ -4,53 +4,16 @@
     {
         public string IncreaseFeedometer(Dragon dragon)
         {
-            dragon.Feedometer += FeedometerIncrement(dragon);
+            dragon.Feedometer += CareLevelManager(dragon)[0];
 
             var dragonsmessage = "That was yummy!";
 
             return dragonsmessage;
         }
 
-        private static int FeedometerIncrement(Dragon dragon)
-        {
-            var ageGroup = dragon.AgeGroup.ToString();
-
-            int feedingIncrement;
-
-            switch (ageGroup)
-            {
-                case "Baby":
-                    feedingIncrement = 10;
-                    break;
-
-                case "Child":
-                    feedingIncrement = 15;
-                    break;
-
-                case "Teen":
-                    feedingIncrement = 25;
-                    break;
-
-                case "Adult":
-                    feedingIncrement= 20;
-                    break;
-
-                case "Senior":
-                    feedingIncrement = 15;
-                    break;
-
-                default:
-                    feedingIncrement = 10;
-                    break;
-
-            }
-
-            return feedingIncrement;
-        }
-
         public string IncreaseHappiness(Dragon dragon)
         {
-            dragon.Happiness += 50;
+            dragon.Happiness += CareLevelManager(dragon)[1];
 
             var dragonsmessage = "I love you!";
 
@@ -60,8 +23,63 @@
         public void ProgressLifeSettings(Dragon dragon)
         {
             dragon.Age += 0.1;
-            dragon.Feedometer--;
-            dragon.Happiness--;
+            dragon.Feedometer -= CareLevelManager(dragon)[2];
+            dragon.Happiness -= CareLevelManager(dragon)[3];
+        }
+
+        public int[] CareLevelManager(Dragon dragon)
+        {
+            var ageGroup = dragon.AgeGroup.ToString();
+
+            int feedometerIncrement, happinessIncrement, hungerIncrement, sadnessIncrement;
+
+            switch (ageGroup)
+            {
+                case "Baby":
+                    feedometerIncrement = 10;
+                    happinessIncrement = 15;
+                    hungerIncrement  = 3;
+                    sadnessIncrement = 4;
+                    break;
+
+                case "Child":
+                    feedometerIncrement = 15;
+                    happinessIncrement = 20;
+                    hungerIncrement  = 2;
+                    sadnessIncrement = 3;
+                    break;
+
+                case "Teen":
+                    feedometerIncrement = 25;
+                    happinessIncrement = 35;
+                    hungerIncrement  = 5;
+                    sadnessIncrement = 1;
+                    break;
+
+                case "Adult":
+                    feedometerIncrement= 20;
+                    happinessIncrement = 25;
+                    hungerIncrement  = 2;
+                    sadnessIncrement = 2;
+                    break;
+
+                case "Senior":
+                    feedometerIncrement = 15;
+                    happinessIncrement = 30;
+                    hungerIncrement  = 1;
+                    sadnessIncrement = 1;
+                    break;
+
+                default:
+                    feedometerIncrement = 10;
+                    happinessIncrement = 15;
+                    hungerIncrement  = 1;
+                    sadnessIncrement = 1;
+                    break;
+
+            }
+
+            return new[] { feedometerIncrement, happinessIncrement, hungerIncrement, sadnessIncrement };
         }
     }
 }
