f74d32cb0402fda927fa0c97180395a29d386366
adamhrapkowicz <wolsztyn@hotmail.com>
2023-04-06 14:38:36 +0100

    Feat: css style added to improve the look in index.html(font made bold for better visibility, background added), dragonPic is now replace by jQuery with another picture if dragon dies.

---
+++ Tamagotchi.Site/Program.cs
@@ -0,0 +1,15 @@
+namespace Tamagotchi.Site
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
